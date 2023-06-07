f3d3f2380a8d1b87542ec8941085895c1416f889
tomer <tomer.gil@mail.huji.ac.il>
2023-06-07 21:27:17 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    np.random.seed(0)
+    df = pd.read_csv('agoda_cancellation_train.csv')
+    X, y = df.drop("cancellation_datetime", axis=1), df["cancellation_datetime"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+    print()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
