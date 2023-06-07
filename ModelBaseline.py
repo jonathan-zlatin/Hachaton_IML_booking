2ade7a4e7b3413f8906794697dcce86612145404
Yael Yanuka <yael.yanuka@eko.com>
2023-06-08 00:05:31 +0300

    create baseline model

--- ModelBaseline.py
+++ ModelBaseline.py
@@ -2,15 +2,15 @@ from sklearn import tree
 
 
 class BaselineModel:
-    def __init__(self):
-        self.clf = tree.DecisionTreeClassifier()
-        self.max_depth = 5
+    def __init__(self, max_depth):
+        self.clf = tree.DecisionTreeClassifier(max_depth=max_depth)
+        self.max_depth = max_depth
 
     def fit(self, X, y):
         self.clf = self.clf.fit(X, y)
 
-    def predict(self, y):
-        return self.clf.predict(y)
+    def predict(self, X):
+        return self.clf.predict(X)
 
-    def loss(self,X, y):
+    def loss(self, X, y):
         return self.clf.score(X, y)
