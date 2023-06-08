aea0347c12ad7a63ac1e51dca21a7b8f42da9002
alon0uzan <105119685+alon0uzan@users.noreply.github.com>
2023-06-08 22:55:10 +0300

    commite changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+import utils
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
@@ -21,13 +22,13 @@ def preprocess(X: pd.DataFrame, y: pd.DataFrame):
     # Convert the 'checkin_date' and 'booking_datetime' columns to datetime type
     X['checkin_date'] = pd.to_datetime(X['checkin_date'])
     X['booking_datetime'] = pd.to_datetime(X['booking_datetime'])
-
+    X = utils.convert_cancellation_code_to_columns(X)
     # Calculate the difference in days between 'checkin_date' and 'booking_datetime'
     X['days_before_checkin'] = (X['checkin_date'] - X['booking_datetime']).dt.days + 1
 
     X.drop(["h_customer_id", "customer_nationality", "origin_country_code",
             "language", "original_payment_currency", "is_user_logged_in",
-            "cancellation_policy_code", "hotel_area_code", "booking_datetime", "checkin_date", "checkout_date",
+            "hotel_area_code", "booking_datetime", "checkin_date", "checkout_date",
             "hotel_live_date", "request_nonesmoke", 'request_latecheckin', "request_highfloor", "request_twinbeds",
             "request_airport", "request_earlycheckin", "request_largebed", "hotel_country_code",
             "guest_nationality_country_name",
@@ -37,11 +38,11 @@ def preprocess(X: pd.DataFrame, y: pd.DataFrame):
                                                                                "Guest House / Bed & Breakfast",
                                                                                "Hostel", "Resort"]),
                                             X["accommadation_type_name"], 'other')
-    X["charge_option"] = np.where(X["accommadation_type_name"] == "Pay at Check-in", "Pay Later",
-                                  X["accommadation_type_name"])
+    X["charge_option"] = np.where(X["charge_option"] == "Pay at Check-in", "Pay Later",
+                                  X["charge_option"])
     X["original_payment_method"] = np.where(X["original_payment_method"].isin(["Visa",
                                                                            "MasterCard", "American Express"]),
-                                          X["accommadation_type_name"], 'other')
+                                          X["original_payment_method"], 'other')
 
     X = pd.get_dummies(X, columns=["accommadation_type_name", "charge_option", "original_payment_type",
                                    "original_payment_method"])
@@ -60,6 +61,7 @@ def replace_none_with_samples(data):
         data.loc[data[column].isna(), column] = samples
 
     return data
+
 
 if __name__ == '__main__':
     np.random.seed(0)
@@ -91,12 +93,15 @@ if __name__ == '__main__':
     cost_of_cancellation_predictions.to_csv("agoda_cost_of_cancellation.csv", index=False)
 
     # Q3
-    # for param in X[0]:
-    #     pearson_corr = np.cov(X[param], y)[0, 1] / (np.std(X[param]) * np.std(y))
-    #
-    #     curr_fig = px.scatter(pd.DataFrame({'x': X[param], 'y': y}), x="x", y="y", trendline="ols",
-    #                           title=f"Correlation between {param} and the response <br>"
-    #                           f"Pearson Correlation {pearson_corr}",
-    #                           labels={"x": f"{param}", "y": "Response(Price)"})
-    #     curr_fig.write_image(f"/pearson_corr_of_{param}.png")
+    pearsons = list()
+    params = list()
+    for param in X:
+        pearson_corr = np.cov(X[param], y)[0, 1] / (np.std(X[param]) * np.std(y))
+        pearsons.append(pearson_corr)
+        params.append(param)
+        print(f"The Pearson corr of {param} is : {pearson_corr}")
 
+    max_ = np.argmax(pearsons)
+    min_ = np.argmin(pearsons)
+    print(f"And The Winner Is {params[max_]} with pearson corr of : {np.max(pearsons)}")
+    print(f"And The Losser Is {params[min_]} with pearson corr of : {np.min(pearsons)}")
