6088462f44b1a7ebedcc77e31a3e238e1771aae8
tomer <tomer.gil@mail.huji.ac.il>
2023-06-08 15:31:41 +0300

    Done parsing cancellation code

--- utils.py
+++ utils.py
@@ -1,18 +1,20 @@
+import pandas as pd
 import re
+import numpy as np
 
-def format_N2P(num: int, unit: str, invitation: pd.Series)
-    price = invitation["original_selling_amount"]
-
+def format_N2P(nights: int, invitation: pd.Series):
     # Calculate the number of days the customer ordered
     # change to dt format
-    invitation['checkin_date'] = invitation.to_datetime(invitation['checkin_date'])
-    invitation['checkout_date'] = invitation.to_datetime(invitation['checkout_date'])
+    invitation['checkin_date'] = pd.to_datetime(invitation['checkin_date'])
+    invitation['checkout_date'] = pd.to_datetime(invitation['checkout_date'])
 
-    days_stay = (invitation['checkout_date'] - invitation['checkin_date']).dt.days
+    days_stay = (invitation['checkout_date'] - invitation['checkin_date']).days
+
+    price = invitation["original_selling_amount"]
     price_pr_night = price / days_stay
 
     # calculate the ratio price
-    total_pay = price_pr_night * num
+    total_pay = price_pr_night * nights
     percent = int((total_pay / price) * 100)
     return percent
 
@@ -24,7 +26,7 @@ def validate_format(policy_format: str) -> bool:
     return True
 
 
-def parse_cancellation_policy(policy: str) -> float:
+def parse_cancellation_policy_old(policy: str) -> float:
     if not validate_format(policy):
         return 0
 
@@ -41,13 +43,53 @@ def parse_cancellation_policy(policy: str) -> float:
         cancelled_after_x = cancelled_before_x[1]
 
 
-def parse(policy: str) -> tuple:
-    pattern = re.compile(r"([0-9]{1,3})(D)([0-9]{1,3})([N, P])")
-    if (match := pattern.match(policy)) is None:
-        return None, None, None
-    return int(match.group(1)), int(match.group(3)), match.group(4)
+def parse_policy_part(policy: str) -> tuple:
+    cancel_ahead_pattern = re.compile(r"([0-9]{1,3})(D)([0-9]{1,3})([N,P])")
+    no_show_pattern = re.compile(r"([0-9]{1,3})([N,P])")
+    if (first_match := cancel_ahead_pattern.match(policy)) is None:
+        if (second_match := no_show_pattern.match(policy)) is None:
+            return None, None, None
+        return int(second_match.group(1)), second_match.group(2)
+    return int(first_match.group(1)), int(first_match.group(3)), first_match.group(4)
+
+
+def parse_cancellation_policy(policy: str) -> list[tuple]:
+    policy_parts = policy.split("_")
+    return [
+        parse_policy_part(policy_part) for policy_part in policy_parts
+    ]
+
+
+def convert_policy_part_to_percent(policy_part: tuple, row: pd.Series):
+    unit = policy_part[-1]
+    if unit == "N":
+        if len(policy_part) == 2:
+            return format_N2P(policy_part[0], row), "P"
+        return policy_part[0], format_N2P(policy_part[1], row), "P"
+    return policy_part
+
+
+def calculate_score(row: pd.Series) -> int:
+    cancellation_policy = parse_cancellation_policy(row["cancellation_policy_code"])
+    cancellation_policy = np.array([
+        convert_policy_part_to_percent(policy_part, row)
+        for policy_part in cancellation_policy
+    ])
+    cancellation_policy = list(filter(
+        lambda policy_part: len(policy_part) == 3
+    , cancellation_policy))
+    return np.sum([
+        d * p for d, p, _ in cancellation_policy
+    ])
 
 
 if __name__ == "__main__":
-    res = parse("1D1N")
-    print()
+    res = parse_policy_part("1D1N")
+    df = pd.read_csv('agoda_cancellation_train.csv')
+    row = df.iloc[7]
+    parsed = parse_cancellation_policy(row["cancellation_policy_code"])
+    parsed = parsed[0]
+    result = format_N2P(parsed[1], row)
+    print("{}P".format(result))
+    score = calculate_score(row)
+    print(score)
